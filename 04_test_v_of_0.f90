d8a8dfe50f2ab13c6eb9f883536f87b0aa1ee9d0
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-05-05 00:16:38 +0700

    Calculation of v_of_0

---
+++ 04_test_v_of_0.f90
@@ -0,0 +1,20 @@
+PROGRAM test_dfft
+  IMPLICIT NONE 
+
+  CALL prepare_all()
+
+  CALL info_scf()
+END PROGRAM 
+
+
+SUBROUTINE info_scf()
+  
+  USE scf, ONLY : v_of_0
+
+  IMPLICIT NONE 
+  
+  WRITE(*,*)
+  WRITE(*,*) 'v_of_0 = ', v_of_0
+  WRITE(*,*)
+
+END SUBROUTINE 
