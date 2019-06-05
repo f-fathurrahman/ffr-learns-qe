882debc9e18bd41c44dfa3e842187386f47f8301
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2019-06-05 10:54:09 +0700

    Test symvector

--- 05_test_symmetry.f90
+++ 05_test_symmetry.f90
@@ -7,18 +7,82 @@ PROGRAM main
   CALL test_symmetry()
 END PROGRAM 
 
+SUBROUTINE my_symvector(nat, vect)
+  USE kinds, ONLY: DP
+  USE cell_base,  ONLY : at, bg
+  USE symm_base,  ONLY : s, nsym, irt
+
+
+     !-----------------------------------------------------------------------
+     ! Symmetrize a function f(i,na), i=cartesian component, na=atom index
+     ! e.g. : forces (in cartesian axis) 
+     !
+     IMPLICIT NONE
+     !
+     INTEGER, INTENT(IN) :: nat
+     REAL(DP), intent(INOUT) :: vect(3,nat)
+     !
+     INTEGER :: na, isym, nar
+     REAL(DP) :: dv(3)
+     REAL(DP), ALLOCATABLE :: work (:,:)
+     !
+     IF (nsym == 1) RETURN
+     !
+     ALLOCATE (work(3,nat))
+     !
+     ! bring vector to crystal axis
+     !
+     DO na = 1, nat
+        work(:,na) = vect(1,na)*at(1,:) + &
+                     vect(2,na)*at(2,:) + &
+                     vect(3,na)*at(3,:)
+     END DO
+     !
+     ! symmetrize in crystal axis
+     !
+     vect (:,:) = 0.0_dp
+     DO na = 1, nat
+        WRITE(*,*)
+        WRITE(*,*) 'before:', work(:,na)
+        DO isym = 1, nsym
+           nar = irt (isym, na)
+           dv(:) = s(:,1,isym)*work(1,nar) + &
+                   s(:,2,isym)*work(2,nar) + &
+                   s(:,3,isym)*work(3,nar)
+           vect(:, na) = vect(:,na) + dv(:)
+           WRITE(*,*) 'dv = ', dv(:)
+        END DO
+        WRITE(*,*) 'after:', vect(:,na)
+     END DO
+     work (:,:) = vect (:,:) / DBLE(nsym)
+     !
+     ! bring vector back to cartesian axis
+     !
+     DO na = 1, nat
+        vect(:,na) = work(1,na)*bg(:,1) + &
+                     work(2,na)*bg(:,2) + &
+                     work(3,na)*bg(:,3)
+     END DO
+     !
+     DEALLOCATE (work)
+     !
+END SUBROUTINE
+
+
 
 SUBROUTINE test_symmetry()
   
-  USE symm_base, ONLY : nrot, fft_fact, ft, ftau, s, nsym
+  USE symm_base, ONLY : nrot, fft_fact, ft, ftau, s, nsym, irt
+  USE symme, ONLY: symvector
   USE gvect, ONLY : ngm, g
   USE my_symme, ONLY: Ngs, my_sym_rho_init_shells
   USE cell_base, ONLY: at, alat, tpiba
 
   IMPLICIT NONE 
-  INTEGER :: isym
+  INTEGER :: isym, ia
   LOGICAL :: non_symmorphic(48)
   REAL(8) :: ft_(3,48), sg(3), arg
+  REAL(8) :: v(3,2)
 
   WRITE(*,*)
   WRITE(*,*) 'nrot = ', nrot
@@ -77,4 +141,20 @@ SUBROUTINE test_symmetry()
 
   WRITE(*,*) 'tpiba = ', tpiba
   WRITE(*,*) g(:,2)*tpiba
+
+  DO isym = 1,nsym
+    DO ia = 1,size(irt,2)
+      WRITE(*,'(1x,I4,x)',advance='no') irt(isym,ia)
+    ENDDO 
+    WRITE(*,*)
+  ENDDO 
+
+  v(:,1) = (/ -0.1d0, 1.d0, 0.d0 /)
+  v(:,2) = (/  0.0d0, 0.1d0, 1.d0 /)
+
+  CALL my_symvector(2, v)
+  WRITE(*,*) v(:,1)
+  WRITE(*,*) v(:,2)
+
+
 END SUBROUTINE 
