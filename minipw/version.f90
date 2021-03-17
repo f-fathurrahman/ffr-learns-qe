4c09353d0a3bc9a1edd7679e3b1eafe55480fd4e
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2021-03-17 12:47:27 +0700

    Add files for pw.x to minipw

---
+++ minipw/version.f90
@@ -0,0 +1,17 @@
+!
+! Copyright (C) 2003-2018 Quantum ESPRESSO group
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+!----------------------------------------------------------------------------
+MODULE global_version
+  !
+  IMPLICIT NONE
+  !
+  SAVE
+  !
+#include "version.h"
+  !
+END MODULE global_version
