c9ea60fde84b75bd114cbc1601dad022d5d05323
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-02-12 19:21:50 +0700

    Improve output

--- minipw-6.6/20_aainit.f90
+++ minipw-6.6/20_aainit.f90
@@ -60,12 +60,15 @@ SUBROUTINE my_aainit(lli)
   ALLOCATE( lpl(nlx,nlx,mx) )  ! list of combined angular momenta  LM
   
   WRITE(*,*) 'lmaxkb = ', lmaxkb
-  WRITE(*,*) 'lli = ', lli
+  WRITE(*,*) 'lli (maximum li considered) = ', lli
 
-  WRITE(*,*) 'lmaxx = ', lmaxx
-  WRITE(*,*) 'nlx   = ', nlx
-  WRITE(*,*) 'lqmax = ', lqmax
-  WRITE(*,*) 'mx    = ', mx
+  WRITE(*,*)
+  WRITE(*,*) 'Some hardcoded parameters'
+  WRITE(*,*) '-------------------------'
+  WRITE(*,*) 'lmaxx (max nonlocal proj angmom) = ', lmaxx
+  WRITE(*,*) 'nlx (max no. of combined angmom) = ', nlx
+  WRITE(*,*) 'lqmax (max no. of angmom of Q)   = ', lqmax
+  WRITE(*,*) 'mx (max magnetic angmom of Q)    = ', mx
 
   ! Clebsch-Gordan coefficients for spherical harmonics
   ALLOCATE( ap(lqmax*lqmax,nlx,nlx) )
@@ -91,6 +94,11 @@ SUBROUTINE my_aainit(lli)
 
   ! generate an array of random vectors (uniform deviate on unitary sphere)
   CALL my_gen_rndm_r(llx, r, rr)
+  WRITE(*,*)
+  WRITE(*,*) 'Randomly generated unit vectors'
+  DO li = 1, llx
+    WRITE(*,'(1x,I4,4F18.10)') li, r(1,li), r(2,li), r(3,li), rr(li)
+  ENDDO 
 
   ! generate the real spherical harmonics for the array: ylm(ir,lm)
   CALL ylmr2(llx, llx, r, rr, ylm)
@@ -99,13 +107,15 @@ SUBROUTINE my_aainit(lli)
   CALL invmat(llx, ylm, mly)
 
   ! for each li,lj compute ap(l,li,lj) and the indices, lpx and lpl
+  WRITE(*,*)
   DO li = 1, lli*lli
+    WRITE(*,*)
     DO lj = 1, lli*lli
       lpx(li,lj) = 0
       DO l = 1, llx
         ap(l,li,lj) = my_compute_ap(l,li,lj,llx,ylm,mly)
         IF( abs(ap(l,li,lj) ) > 1.d-3 ) THEN 
-          lpx(li,lj) = lpx(li,lj) + 1
+          lpx(li,lj) = lpx(li,lj) + 1 ! increment
           IF(lpx(li,lj) > mx) CALL upf_error('aainit', 'mx dimension too small', lpx(li,lj))
           lpl(li,lj,lpx(li,lj)) = l
           WRITE(*,'(1x,3I4,F18.10)') l, li, lj, ap(l, li, lj)          
@@ -118,6 +128,8 @@ SUBROUTINE my_aainit(lli)
   DEALLOCATE(ylm)
   DEALLOCATE(rr)
   DEALLOCATE(r)
+  DEALLOCATE(lpl)
+  DEALLOCATE(lpx)
 
   RETURN 
 END SUBROUTINE 
@@ -145,7 +157,7 @@ FUNCTION my_compute_ap(l,li,lj,llx,ylm,mly)
     
   my_compute_ap = 0.0_DP
   DO ir = 1,llx
-     my_compute_ap = my_compute_ap + mly(l,ir)*ylm(ir,li)*ylm(ir,lj)
+    my_compute_ap = my_compute_ap + mly(l,ir)*ylm(ir,li)*ylm(ir,lj)
   ENDDO
     
   RETURN 
@@ -154,51 +166,50 @@ END FUNCTION
 
 
 !-----------------------------------------------------------------------
-SUBROUTINE my_gen_rndm_r(llx,r,rr)
+SUBROUTINE my_gen_rndm_r(llx, r, rr)
 !-----------------------------------------------------------------------
   ! - generate an array of random vectors (uniform deviate on unitary sphere)
   !
   USE upf_kinds, ONLY : DP
   USE upf_const,  ONLY: tpi
   IMPLICIT NONE 
-    !
-    ! first the I/O variables
-    !
-    integer :: llx   ! input: the dimension of r and rr
-    real(DP) :: &
-         r(3,llx),  &! output: an array of random vectors
-         rr(llx)     ! output: the norm of r
-    !
-    ! here the local variables
-    !
-    integer  :: ir
-    real(DP) :: costheta, sintheta, phi
-    real(DP) :: my_randy
-    
-    do ir = 1, llx
-       costheta = 2.0_DP * my_randy() - 1.0_DP
-       sintheta = SQRT ( 1.0_DP - costheta*costheta)
-       phi = tpi * my_randy()
-       r (1,ir) = sintheta * cos(phi)
-       r (2,ir) = sintheta * sin(phi)
-       r (3,ir) = costheta
-       rr(ir)   = 1.0_DP
-    enddo
-    
-    return
-end subroutine
+  
+  !
+  ! first the I/O variables
+  !
+  INTEGER :: llx   ! input: the dimension of r and rr
+  REAL(DP) :: r(3,llx) ! output: an array of random vectors
+  REAL(DP) :: rr(llx)     ! output: the norm of r
+  !
+  ! here the local variables
+  !
+  INTEGER :: ir
+  REAL(DP) :: costheta, sintheta, phi
+  REAL(DP) :: my_randy ! FUNCTION
+
+  DO ir = 1, llx
+    costheta = 2.0_DP * my_randy() - 1.0_DP
+    sintheta = SQRT ( 1.0_DP - costheta*costheta)
+    phi = tpi * my_randy()
+    r(1,ir) = sintheta * cos(phi)
+    r(2,ir) = sintheta * sin(phi)
+    r(3,ir) = costheta
+    rr(ir)   = 1.0_DP
+  ENDDO 
+  RETURN 
+END SUBROUTINE 
 
 !------------------------------------------------------------------------
 FUNCTION my_randy()
-  !------------------------------------------------------------------------
+!------------------------------------------------------------------------
   !   
   ! x=randy(n): reseed with initial seed idum=n ( 0 <= n <= ic, see below)
   !             if randy is not explicitly initialized, it will be
   !             initialized with seed idum=0 the first time it is called
   ! x=randy() : generate uniform real(DP) numbers x in [0,1]
   !   
-  use upf_kinds, only : DP
-  implicit none
+  USE upf_kinds, only : DP
+  IMPLICIT NONE 
   !
   REAL(DP) :: my_randy
   !INTEGER, optional    :: irand
@@ -230,10 +241,11 @@ FUNCTION my_randy()
   ENDIF
 
   j = 1 + (ntab*iy)/m
-  IF( j > ntab .OR. j <  1 ) call upf_error('my_randy', 'j out of range', ABS(j)+1)
+  IF( j > ntab .OR. j <  1 ) CALL upf_error('my_randy', 'j out of range', ABS(j)+1)
   iy = ir(j)
   my_randy = iy*rm
   idum = mod(ia*idum+ic,m)
   ir(j) = idum
   RETURN
-END FUNCTION
+END FUNCTION 
+
