d4a990b7b559ad2b946595421d007e80082bf12a
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-07-16 15:22:37 +0700

    Explicit fft_interpolate_real

--- minipw-6.6/srclib/fft_interpolate.f90
+++ minipw-6.6/srclib/fft_interpolate.f90
@@ -5,8 +5,10 @@
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
-!
-subroutine fft_interpolate_real (dfft_in, v_in, dfft_out, v_out )
+
+!----------------------------------------------------------------
+subroutine fft_interpolate_real( dfft_in, v_in, dfft_out, v_out )
+!----------------------------------------------------------------
   !
   !   This subroutine interpolates an array  v_in   defined on fft grid  dfft_in
   !                           to   an array  v_out  defined on fft grid  dfft_out
@@ -17,44 +19,58 @@ subroutine fft_interpolate_real (dfft_in, v_in, dfft_out, v_out )
   USE fft_interfaces, ONLY : fwfft, invfft
   ! I/O variables
   TYPE(fft_type_descriptor), INTENT(IN) :: dfft_in, dfft_out
-  REAL(DP),INTENT(IN)  :: v_in (:) !dfft_in%nnr)
-  REAL(DP),INTENT(OUT) :: v_out (:) !dfft_out%nnr)
+  REAL(DP), INTENT(IN) :: v_in(:) !dfft_in%nnr)
+  REAL(DP), INTENT(OUT) :: v_out(:) !dfft_out%nnr)
   ! local variables
   INTEGER :: ngm
   COMPLEX(DP), ALLOCATABLE :: aux_in (:), aux_out (:)
 
   call start_clock ('interpolate')
 
-  IF (dfft_out%grid_id == dfft_in%grid_id) THEN
+  write(*,*)
+  write(*,*) 'fft_interpolate_real: shape v_in = ', shape(v_in)
+  write(*,*) 'fft_interpolate_real: shape v_out = ', shape(v_out)
+  write(*,*)
+
+
+  IF( dfft_out%grid_id == dfft_in%grid_id ) THEN
      
-     write(*,*) 'fft_interpolate: simply copy'
-     
-     v_out (1:dfft_in%nnr) = v_in (1:dfft_in%nnr)
+    write(*,*) 'fft_interpolate_real: simply copy' 
+    v_out (1:dfft_in%nnr) = v_in (1:dfft_in%nnr)
 
   ELSE
      
-     write(*,*) 'fft_interpolate: two different grids'
-     write(*,*) 'dfft_in%nnr = ', dfft_in%nnr
-     write(*,*) 'dfft_out%nnr = ', dfft_out%nnr
+     write(*,*) 'fft_interpolate_real: two different grids'
+     write(*,*) 'fft_interpolate_real: dfft_in%nnr = ', dfft_in%nnr
+     write(*,*) 'fft_interpolate_real: dfft_out%nnr = ', dfft_out%nnr
 
      if (dfft_in%lgamma .neqv. dfft_out%lgamma) &
         call fftx_error__ ('fft_interpolate_real','two grids with inconsistent lgamma values', 1)
 
-     ALLOCATE(aux_in( dfft_in%nnr), aux_out(dfft_out%nnr))
+     ALLOCATE( aux_in(dfft_in%nnr) )
+     ALLOCATE( aux_out(dfft_out%nnr) )
 
+     ! Copy all data to aux_in
      aux_in(1:dfft_in%nnr) = v_in(1:dfft_in%nnr)
 
+     ! To G-space
      CALL fwfft('Rho', aux_in, dfft_in)
 
+     ! Zero out aux_out
      aux_out(1:dfft_out%nnr) = (0.d0, 0.d0)
 
+     ! find the minimum of ngm for both FFT grids
      ngm = min(dfft_in%ngm, dfft_out%ngm)
-     write(*,*) 'fft_interpolate ngm = ', ngm
+     write(*,*) 'fft_interpolate_real: ngm = ', ngm
 
-     aux_out(dfft_out%nl (1:ngm) ) = aux_in(dfft_in%nl (1:ngm) )
+     ! The indices dfft_in%nl(1:ngm)
+     ! Copy aux_in to aux_out
+     aux_out( dfft_out%nl(1:ngm) ) = aux_in( dfft_in%nl(1:ngm) )
+     
      ! additional work for gamma-trick
-     IF(dfft_in%lgamma) aux_out (dfft_out%nlm (1:ngm) ) = aux_in (dfft_in%nlm (1:ngm) )
+     IF(dfft_in%lgamma) aux_out( dfft_out%nlm(1:ngm) ) = aux_in( dfft_in%nlm(1:ngm) )
 
+     ! Back to R-space
      CALL invfft('Rho', aux_out, dfft_out)
 
      v_out(1:dfft_out%nnr) = aux_out(1:dfft_out%nnr)
@@ -68,8 +84,11 @@ subroutine fft_interpolate_real (dfft_in, v_in, dfft_out, v_out )
   return
 
 end subroutine fft_interpolate_real
-!
+
+
+!-------------------------------------------------------------------
 subroutine fft_interpolate_complex (dfft_in, v_in, dfft_out, v_out )
+!-------------------------------------------------------------------
   !
   !   This subroutine interpolates an array  v_in   defined on fft grid  dfft_in
   !                           to   an array  v_out  defined on fft grid  dfft_out
