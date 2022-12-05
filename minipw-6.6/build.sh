2b119be55e518b2f78230651308b8fcb7e30b1aa
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2022-12-05 16:28:51 +0700

    Fix rpath, need to specify rpath to create libqemain.so

--- minipw-6.6/build.sh
+++ minipw-6.6/build.sh
@@ -2,12 +2,16 @@
 
 INC="-I./srclib"
 QE_HOME="/home/efefer/WORKS/my_github_repos/ffr-learns-qe/minipw-6.6/srclib"
-LIB="-Wl,-rpath,${QE_HOME} -L${QE_HOME} -lqemain -lblas -llapack"
+
+LIB="-Wl,-rpath ${QE_HOME} -L${QE_HOME} -lqemain -lblas -llapack"
+
 LIBXC_HOME="/home/efefer/mysoftwares/libxc-4.3.4dyn"
-LIB_LIBXC="-Wl,-rpath,${LIBXC_HOME} -L${LIBXC_HOME}/lib -lxcf90 -lxcf03 -lxc"
+LIB_LIBXC="-Wl,-rpath ${LIBXC_HOME}/lib -L${LIBXC_HOME}/lib -lxcf03 -lxc"
 
 basn=`basename $1 .f90`
 rm -vf $basn.x
 
-gfortran -Wall $INC $1 $LIB $LIB_LIBXC -o $basn.x
+# FIXME: rpath for Libxc is not
+
+gfortran -Wall $INC $1 $LIB_LIBXC $LIB -o $basn.x
 echo "Executable: $basn.x"
