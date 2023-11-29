7ce30b3d660f9eaf544df0409eda53a7eb584520
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2023-11-29 21:13:06 +0700

    Clean up

--- minipw-6.6/32_debug_paw_gcxc.f90
+++ minipw-6.6/32_debug_paw_gcxc.f90
@@ -133,7 +133,7 @@ SUBROUTINE debug_PAW_gcxc_potential()
   write(*,*) 'shape rho_lm = ', shape(rho_lm)
 
   !
-  write(*,*) 'X = ', i%a, i_what, sgn*energy
+  write(*,*) 'X (in Ha) = ', i%a, i_what, sgn*energy*0.5d0
   energy_tot = energy_tot + sgn*energy
   e_cmp(ia, XC, i_what) = sgn*energy
   !
@@ -332,144 +332,143 @@ SUBROUTINE my_PAW_gcxc_potential(i, rho_lm, rho_core, v_lm, energy)
 
     !
     !
-    ELSEIF ( nspin_mag == 2 .OR. nspin_mag == 4 ) THEN
-        !
-        ALLOCATE( gradx(3,i%m,2) )
-        ALLOCATE( r_vec(i%m,2) )
-        ALLOCATE( v2cud(i%m) )
-        !
-        !   this is the \sigma-GGA case
-        !
-        DO ix = 1, rad(i%t)%nx
-           !
-           CALL PAW_lm2rad( i, ix, rhoout_lm, rho_rad, nspin_gga )
-           CALL PAW_gradient( i, ix, rhoout_lm, rho_rad, rho_core,grad2, grad )
-           !
-           DO k = 1, i%m
-               !
-               ! Prepare the necessary quantities
-               ! rho_core is considered half spin up and half spin down:
-               co2 = rho_core(k)/2
-               ! than I build the real charge dividing by r**2
-               r_vec(k,1) = rho_rad(k,1)*g(i%t)%rm2(k) + co2
-               r_vec(k,2) = rho_rad(k,2)*g(i%t)%rm2(k) + co2
-               !
-               !
-               gradx(:,k,1) = grad(k,:,1)
-               gradx(:,k,2) = grad(k,:,2)
-           ENDDO
-           !
-           CALL xc_gcx( i%m, 2, r_vec, gradx, sx, sc, v1x, v2x, v1c, v2c, v2cud )
-           !
-           DO k = 1, i%m
-              !
-              e_rad(k) = e2*(sx(k)+sc(k))*g(i%t)%r2(k)
-              !
-              ! first term of the gradient correction : D(rho*Exc)/D(rho)
-              gc_rad(k,ix,1)  = (v1x(k,1)+v1c(k,1)) !*g(i%t)%rm2(k)
-              gc_rad(k,ix,2)  = (v1x(k,2)+v1c(k,2)) !*g(i%t)%rm2(k)
-              !
-              ! h contains D(rho*Exc)/D(|grad rho|) * (grad rho) / |grad rho|
-              ! h_rad(k,:,ix,1) =( (v2xup_vec(k)+v2c)*grad(k,:,1)+v2c*grad(k,:,2) )*g(i%t)%r2(k)
-              ! h_rad(k,:,ix,2) =( (v2xdw_vec(k)+v2c)*grad(k,:,2)+v2c*grad(k,:,1) )*g(i%t)%r2(k)
-              h_rad(k,:,ix,1) =( (v2x(k,1)+v2c(k,1))*grad(k,:,1) + &
-                                  v2cud(k)*grad(k,:,2) )*g(i%t)%r2(k)
-              h_rad(k,:,ix,2) =( (v2x(k,2)+v2c(k,2))*grad(k,:,2) + &
-                                  v2cud(k)*grad(k,:,1) )*g(i%t)%r2(k)
-              !
-           ENDDO
-           !
-           ! integrate energy (if required)
-           ! NOTE: this integration is duplicated for every spin, FIXME!
-           !IF (PRESENT(energy)) THEN
-               CALL simpson( i%m, e_rad, g(i%t)%rab, e )
-               egcxc_of_tid(mytid) = egcxc_of_tid(mytid) + e * rad(i%t)%ww(ix)
-           !ENDIF
-           !
-        ENDDO ! ix
-        !
-        DEALLOCATE( gradx )
-        DEALLOCATE( r_vec )
-        DEALLOCATE( v2cud )
-        !
-    ELSE spin
+  ELSEIF ( nspin_mag == 2 .OR. nspin_mag == 4 ) THEN
     !
-        CALL errore( 'PAW_gcxc_v', 'unknown spin number', 2 )
-    ENDIF spin
-    
-    DEALLOCATE( e_rad )
+    ALLOCATE( gradx(3,i%m,2) )
+    ALLOCATE( r_vec(i%m,2) )
+    ALLOCATE( v2cud(i%m) )
     !
-    DEALLOCATE( rho_rad )
-    DEALLOCATE( grad  )
-    DEALLOCATE( grad2 )
-
-    ! accumulate sum of egcxc to energy
-    e_gcxc = SUM(egcxc_of_tid)
-    energy = energy + e_gcxc
-
-    DEALLOCATE( egcxc_of_tid )
+    !   this is the \sigma-GGA case
     !
-    ! convert the first part of the GC correction back to spherical harmonics
-    CALL PAW_rad2lm( i, gc_rad, gc_lm, i%l, nspin_gga )
-    
-    !
-    write(*,*) 'sum wwylm = ', sum(rad(i%t)%wwylm)
-    write(*,*) 'i%l = ', i%l
-    write(*,*) 'sum gc_rad = ', sum(gc_rad)
-    write(*,*) 'sum abs gc_lm = ', sum(abs(gc_lm))
-    write(*,*) 'shape gc_lm = ', shape(gc_lm)
-    write(*,*) 'gc_rad 1 1 1 = ', gc_rad(1,1,1)
-    write(*,*) 'gc_lm 1 1 1 = ', gc_lm(1,1,1)
-    !
-    ! Note that the expansion into spherical harmonics of the derivative 
-    ! with respect to theta of the spherical harmonics, is very slow to
-    ! converge and would require a huge angular momentum ladd.
-    ! This derivative divided by sin_th is much faster to converge, so
-    ! we divide here before calculating h_lm and keep into account for
-    ! this factor sin_th in the expression of the divergence.
-    !
-    ! ADC 30/04/2009.
-    ! 
     DO ix = 1, rad(i%t)%nx
-       h_rad(1:i%m,3,ix,1:nspin_gga) = h_rad(1:i%m,3,ix,1:nspin_gga)/rad(i%t)%sin_th(ix)
-    ENDDO
-    ! We need the gradient of H to calculate the last part of the exchange
-    ! and correlation potential. First we have to convert H to its Y_lm expansion
-    CALL PAW_rad2lm3( i, h_rad, h_lm, i%l+rad(i%t)%ladd, nspin_gga )
-
-    write(*,*) 'l = ', i%l
-    write(*,*) 'ladd = ', rad(i%t)%ladd
-    write(*,*) 'lmax_loc_add = ', i%l + rad(i%t)%ladd
-    write(*,*) 'sum abs h_rad = ', sum(abs(h_rad))
-    write(*,*) 'sum abs h_lm = ', sum(abs(h_lm))
-    write(*,*) 'h_lm 1 = ', h_lm(1,1,1,1)
-    write(*,*) 'h_lm 2 = ', h_lm(1,2,1,1)
-    write(*,*) 'h_lm 3 = ', h_lm(1,3,1,1)
-
-    write(*,*) 'maxval(abs(h_rad)) 1 = ', maxval(abs(h_rad(:,1,:,:)))
-    write(*,*) 'maxval(abs(h_rad)) 2 = ', maxval(abs(h_rad(:,2,:,:)))
-    write(*,*) 'maxval(abs(h_rad)) 3 = ', maxval(abs(h_rad(:,3,:,:)))
-    write(*,*)
-    write(*,*) 'maxval(abs(h_lm)) 1 = ', maxval(abs(h_lm(:,1,:,:)))
-    write(*,*) 'maxval(abs(h_lm)) 2 = ', maxval(abs(h_lm(:,2,:,:)))
-    write(*,*) 'maxval(abs(h_lm)) 3 = ', maxval(abs(h_lm(:,3,:,:)))
-    write(*,*)
-
-    write(*,*) 'sum sin_th = ', sum(rad(i%t)%sin_th)
-
-    !
-    ! Compute div(H)
-    CALL PAW_divergence( i, h_lm, div_h, i%l+rad(i%t)%ladd, i%l )
-    !                       input max lm --^  output max lm-^
-    
-    write(*,*) 'sum div_h = ', sum(div_h)
-
-    ! Finally sum it back into v_xc
-    DO is = 1,nspin_gga
-      DO lm = 1,i%l**2
-         vout_lm(1:i%m,lm,is) = vout_lm(1:i%m,lm,is) + e2*(gc_lm(1:i%m,lm,is)-div_h(1:i%m,lm,is))
+      !
+      CALL PAW_lm2rad( i, ix, rhoout_lm, rho_rad, nspin_gga )
+      CALL PAW_gradient( i, ix, rhoout_lm, rho_rad, rho_core,grad2, grad )
+      !
+      DO k = 1, i%m
+        !
+        ! Prepare the necessary quantities
+        ! rho_core is considered half spin up and half spin down:
+        co2 = rho_core(k)/2
+        ! than I build the real charge dividing by r**2
+        r_vec(k,1) = rho_rad(k,1)*g(i%t)%rm2(k) + co2
+        r_vec(k,2) = rho_rad(k,2)*g(i%t)%rm2(k) + co2
+        !
+        !
+        gradx(:,k,1) = grad(k,:,1)
+        gradx(:,k,2) = grad(k,:,2)
       ENDDO
+      !
+      CALL xc_gcx( i%m, 2, r_vec, gradx, sx, sc, v1x, v2x, v1c, v2c, v2cud )
+      !
+      DO k = 1, i%m
+        !
+        e_rad(k) = e2*(sx(k)+sc(k))*g(i%t)%r2(k)
+        !
+        ! first term of the gradient correction : D(rho*Exc)/D(rho)
+        gc_rad(k,ix,1)  = (v1x(k,1)+v1c(k,1)) !*g(i%t)%rm2(k)
+        gc_rad(k,ix,2)  = (v1x(k,2)+v1c(k,2)) !*g(i%t)%rm2(k)
+        !
+        ! h contains D(rho*Exc)/D(|grad rho|) * (grad rho) / |grad rho|
+        ! h_rad(k,:,ix,1) =( (v2xup_vec(k)+v2c)*grad(k,:,1)+v2c*grad(k,:,2) )*g(i%t)%r2(k)
+        ! h_rad(k,:,ix,2) =( (v2xdw_vec(k)+v2c)*grad(k,:,2)+v2c*grad(k,:,1) )*g(i%t)%r2(k)
+        h_rad(k,:,ix,1) =( (v2x(k,1)+v2c(k,1))*grad(k,:,1) + &
+                            v2cud(k)*grad(k,:,2) )*g(i%t)%r2(k)
+        h_rad(k,:,ix,2) =( (v2x(k,2)+v2c(k,2))*grad(k,:,2) + &
+                            v2cud(k)*grad(k,:,1) )*g(i%t)%r2(k)
+        !
+      ENDDO
+      !
+      ! integrate energy (if required)
+      ! NOTE: this integration is duplicated for every spin, FIXME!
+      !IF (PRESENT(energy)) THEN
+      CALL simpson( i%m, e_rad, g(i%t)%rab, e )
+      egcxc_of_tid(mytid) = egcxc_of_tid(mytid) + e * rad(i%t)%ww(ix)
+      !ENDIF
+      !
+    ENDDO ! ix
+    !
+    DEALLOCATE( gradx )
+    DEALLOCATE( r_vec )
+    DEALLOCATE( v2cud )
+    !
+  ELSE spin
+    !
+    CALL errore( 'PAW_gcxc_v', 'unknown spin number', 2 )
+  ENDIF spin
+    
+  DEALLOCATE( e_rad )
+  !
+  DEALLOCATE( rho_rad )
+  DEALLOCATE( grad  )
+  DEALLOCATE( grad2 )
+
+  ! accumulate sum of egcxc to energy
+  e_gcxc = SUM(egcxc_of_tid)
+  energy = energy + e_gcxc
+
+  DEALLOCATE( egcxc_of_tid )
+  !
+  ! convert the first part of the GC correction back to spherical harmonics
+  CALL PAW_rad2lm( i, gc_rad, gc_lm, i%l, nspin_gga )
+  
+  !
+  !write(*,*) 'sum wwylm = ', sum(rad(i%t)%wwylm)
+  !write(*,*) 'i%l = ', i%l
+  !write(*,*) 'sum gc_rad = ', sum(gc_rad)
+  !write(*,*) 'sum abs gc_lm = ', sum(abs(gc_lm))
+  !write(*,*) 'shape gc_lm = ', shape(gc_lm)
+  !write(*,*) 'gc_rad 1 1 1 = ', gc_rad(1,1,1)
+  !write(*,*) 'gc_lm 1 1 1 = ', gc_lm(1,1,1)
+  !
+  ! Note that the expansion into spherical harmonics of the derivative 
+  ! with respect to theta of the spherical harmonics, is very slow to
+  ! converge and would require a huge angular momentum ladd.
+  ! This derivative divided by sin_th is much faster to converge, so
+  ! we divide here before calculating h_lm and keep into account for
+  ! this factor sin_th in the expression of the divergence.
+  !
+  ! ADC 30/04/2009.
+  ! 
+  DO ix = 1, rad(i%t)%nx
+     h_rad(1:i%m,3,ix,1:nspin_gga) = h_rad(1:i%m,3,ix,1:nspin_gga)/rad(i%t)%sin_th(ix)
+  ENDDO
+  ! We need the gradient of H to calculate the last part of the exchange
+  ! and correlation potential. First we have to convert H to its Y_lm expansion
+  CALL PAW_rad2lm3( i, h_rad, h_lm, i%l+rad(i%t)%ladd, nspin_gga )
+
+  !write(*,*) 'l = ', i%l
+  !write(*,*) 'ladd = ', rad(i%t)%ladd
+  !write(*,*) 'lmax_loc_add = ', i%l + rad(i%t)%ladd
+  !write(*,*) 'sum abs h_rad = ', sum(abs(h_rad))
+  !write(*,*) 'sum abs h_lm = ', sum(abs(h_lm))
+  !write(*,*) 'h_lm 1 = ', h_lm(1,1,1,1)
+  !write(*,*) 'h_lm 2 = ', h_lm(1,2,1,1)
+  !write(*,*) 'h_lm 3 = ', h_lm(1,3,1,1)
+
+  !write(*,*) 'maxval(abs(h_rad)) 1 = ', maxval(abs(h_rad(:,1,:,:)))
+  !write(*,*) 'maxval(abs(h_rad)) 2 = ', maxval(abs(h_rad(:,2,:,:)))
+  !write(*,*) 'maxval(abs(h_rad)) 3 = ', maxval(abs(h_rad(:,3,:,:)))
+  !write(*,*)
+  !write(*,*) 'maxval(abs(h_lm)) 1 = ', maxval(abs(h_lm(:,1,:,:)))
+  !write(*,*) 'maxval(abs(h_lm)) 2 = ', maxval(abs(h_lm(:,2,:,:)))
+  !write(*,*) 'maxval(abs(h_lm)) 3 = ', maxval(abs(h_lm(:,3,:,:)))
+  !write(*,*)
+  !write(*,*) 'sum sin_th = ', sum(rad(i%t)%sin_th)
+
+  !
+  ! Compute div(H)
+  CALL PAW_divergence( i, h_lm, div_h, i%l+rad(i%t)%ladd, i%l )
+  !                       input max lm --^  output max lm-^
+    
+  !write(*,*) 'sum div_h = ', sum(div_h)
+
+  ! Finally sum it back into v_xc
+  DO is = 1,nspin_gga
+    DO lm = 1,i%l**2
+       vout_lm(1:i%m,lm,is) = vout_lm(1:i%m,lm,is) + e2*(gc_lm(1:i%m,lm,is)-div_h(1:i%m,lm,is))
     ENDDO
+  ENDDO
 
   write(*,*) 'sum abs vout_lm (in Ha) = ', 0.5d0*sum(abs(vout_lm))
 
